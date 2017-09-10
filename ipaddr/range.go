88931fdf5a9395fe2ac42a0de1f4a97a14ea7543
qchen-zh <qchen.zh@gmail.com>
2017-09-10 15:24:58 +0800

    初始文件导入。

---
+++ ipaddr/range.go
@@ -0,0 +1,145 @@
+package ipaddr
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// Range IP范围
+// 仅支持IPv4地址类型。
+type Range struct {
+	netip   net.IP // 网络地址
+	begin   int    // 起始主机号
+	end     int    // 终点主机号
+	current int    // 当前取值
+	mu      sync.Mutex
+}
+
+// NewRange 新建一个范围实例。
+// cidr 为网络IP与子网掩码格式的字符串，如："192.0.2.0/24"
+// 仅支持IPv4。
+// first为起始主机号，last为终点主机号。
+func NewRange(cidr string, begin, end int) (*Range, error) {
+	ipn, err := parse(cidr)
+	if err != nil {
+		return nil, err
+	}
+	err = check(begin, end, ipn.Mask)
+	if err != nil {
+		return nil, err
+	}
+	return &Range{netip: ipn.IP, begin: begin, end: end}, nil
+}
+
+//
+// IPAddrs 获取IP序列的管道。
+// 正常退出后会重置内部游标。
+//
+func (r *Range) IPAddrs(cancel func() bool) <-chan net.Addr {
+	ch := make(chan net.Addr)
+
+	go func() {
+		for {
+			addr := r.next()
+			if cancel() || addr == nil {
+				break
+			}
+			ch <- addr
+		}
+		close(ch)
+		r.Reset()
+	}()
+
+	return ch
+}
+
+//
+// Reset 迭代重置。内部游标归零。
+//
+func (r *Range) Reset() *Range {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.current = 0
+	return r
+}
+
+//
+// 获取下一个IP地址。
+// 到终点后返回nil。
+//
+func (r *Range) next() net.Addr {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if r.current >= r.end {
+		return nil
+	}
+	r.current++
+	return makeIPv4(r.netip, iToBytes(uint32(r.current-1)))
+}
+
+/////////////
+// 私有辅助
+///////////////////////////////////////////////////////////////////////////////
+
+//
+// 网络地址解析。
+//
+func parse(cidr string) (*net.IPNet, error) {
+	ip, ipn, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+	if len(ip) != net.IPv4len {
+		return fmt.Errorf("%s is not IPv4 address", cidr)
+	}
+	return ipn, nil
+}
+
+//
+// 范围合法性检查。
+//
+func check(begin, end int, mask net.IPMask) error {
+	if begin >= end || !validHost(mask, end) {
+		return fmt.Errorf("[%d, %d] host ip range is valid", begin, end)
+	}
+	return nil
+}
+
+//
+// 构造IPv4主机地址。
+//
+func makeIPv4(nip net.IP, host []byte) *net.IPAddr {
+	out := make([]byte, len(nip))
+
+	for i := 0; i < net.IPv4len; i++ {
+		out[i] = nip[i] | host[i]
+	}
+	return &net.IPAddr{IP: net.IP(out)}
+}
+
+//
+// 检查子网段主机号是否在有效范围内。
+//
+func validHost(mask net.IPMask, host int) bool {
+	ones, bits := mask.Size()
+
+	if 1<<uint(bits-ones) <= host {
+		return false
+	}
+	return true
+}
+
+//
+// 整数转为字节序列。
+//
+func iToBytes(n uint32) []byte {
+	out := make([]byte, 4)
+
+	for i := 3; i >= 0; i-- {
+		out[i] = byte(n & 0xff)
+		n >>= 8
+	}
+	return out
+}
---
+++ ipaddr/stack.go
@@ -0,0 +1,128 @@
+package ipaddr
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// Stack IP序列
+// 兼容IPv4和IPv6两种格式。
+// 内部用切片实现，可添加重复的地址。
+type Stack struct {
+	pool []net.Addr
+	mu   sync.Mutex
+}
+
+//
+// NewStack 新建一个序列集。
+//
+func NewStack() *Stack {
+	return &Stack{
+		pool: make([]net.Addr),
+	}
+}
+
+//
+// NewStackN 新建一个序列集。
+// 如果已知集合大小，可传递一个初始值（优化）。
+//
+func NewStackN(sz int) *Stack {
+	return &Stack{
+		pool: make([]net.Addr, sz),
+	}
+}
+
+//
+// Add 添加一个IP地址。
+// 如果地址格式错误，返回error。
+// addr不含ipv6的Zone部分。
+//
+func (s *Stack) Add(addr string) error {
+	ip := net.ParseIP(addr)
+	if ip == nil {
+		return fmt.Errorf("%s is not a valid IP address", addr)
+	}
+	s.mu.Lock()
+	s.pool = append(s.pool, &net.IPAddr{IP: ip})
+	s.mu.Unlock()
+
+	return nil
+}
+
+//
+// AddIP 添加一个IP地址。
+// 可包含ipv6中的Zone字段。
+//
+func (s *Stack) AddIP(ip net.Addr) {
+	s.mu.Lock()
+	s.pool = append(s.pool, ip)
+	s.mu.Unlock()
+}
+
+//
+// AddsIP 添加多个IP地址。
+// 可包含ipv6中的Zone字段。
+//
+func (s *Stack) AddsIP(ips []net.Addr) {
+	s.mu.Lock()
+	s.pool = append(s.pool, ips...)
+	s.mu.Unlock()
+}
+
+//
+// Pop 移除最后添加的几个地址。
+// 返回移除的IP地址集，
+// 若集合已空或删除量超出集合大小返回nil。
+//
+func (s *Stack) Pop(n int) []net.Addr {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	sz = len(s.pool) - n
+
+	if len(s.pool) == 0 || n == 0 || sz < 0 {
+		return nil
+	}
+	as := s.pool[sz:]
+	s.pool = s.pool[:sz]
+
+	return as
+}
+
+//
+// IPAddrs 获取IP序列的管道。
+// 允许地址集在服务期变化（更大的灵活性）。
+//
+func (s *Stack) IPAddrs(cancel func() bool) <-chan net.Addr {
+	ch := make(chan net.Addr)
+
+	go func() {
+		i := 0
+		// 循环内互斥保护
+		for {
+			s.mu.Lock()
+			v := index(s, i)
+			s.mu.Unlock()
+
+			if cancel() || v == nil {
+				break
+			}
+			ch <- v
+			i++
+		}
+		close(ch)
+	}()
+
+	return ch
+}
+
+//
+// 提取集合成员，适应集合动态变化。
+//
+func index(s []net.Addr, i int) net.Addr {
+	if len(s) == 0 || len(s) >= i {
+		return nil
+	}
+	return s[i]
+}
