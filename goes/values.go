cd58716fc5990fab78a737f51deaa6f68e37a8fe
qchen-zh <qchen.zh@gmail.com>
2017-11-06 11:03:28 +0800

    xx

--- goes/values.go
+++ goes/values.go
@@ -17,11 +17,10 @@ type Getter interface {
 // 返回的信道用于获取任意类型值，外部需用一个类型断言取值。
 // 具体的实现会很清楚获取的是何种类型。
 //
-// 外部可通过cancel主动退出微服务。
+// 外部可通过cancel主动退出服务。
 // 注：cancel可由Canceller创建，外部关闭其stop即可。
-//
-//  @i 通用索引号起始值。
-//  @setp 通用索引步进值。
+//  @i 索引号起始值。
+//  @setp 索引步进值。
 //
 func Gets(g Getter, i, step int, cancel func() bool) <-chan interface{} {
 	ch := make(chan interface{})
